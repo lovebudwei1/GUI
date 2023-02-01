52154c244e9ea843b2fcb003366553d8935d5d66
lovebudwei1 <lovebudwei@qq.com>
2023-02-02 05:17:12 +0800

    测试

--- firstGUIdemo.py
+++ firstGUIdemo.py
@@ -1,32 +1,39 @@
 import tkinter
 from tkinter import ttk
 import pymysql
-# db = pymysql.connect(host='localhost',
-#                      user='root',
-#                      password='1234',
-#                      database='mars')
+
+db = pymysql.connect(host='localhost',
+                     user='root',
+                     password='1234',
+                     database='baoding')
+
 
 def change(*args):
-    # cur = db.cursor()
-    # cur.execute("SELECT * FROM typedata where typeid-%s and classe = 2")
-
     global list
-    if mode.get() == "A":
-        print("a")
-        list2.config(values=["you choose A"])
-        list2.current(0)
-    else:
-        print("b")
-        list2['value'] = ["you choose B", "you choose B yes"]
-        list2.current(0)
+    area_data = mode.get()
+    area_data = area_data.split(',')
+    cur.execute("SELECT opid,opname FROM optable where areaid = %s", area_data[0])
+    cur_opname = cur.fetchall()
+    list_opname = []
+    for j in cur_opname:
+        list_opname.append(('%s' + ',' + '%s') % (j[0], j[1]))
+    list2['value'] = list_opname
+    list2.current(0)
 win = tkinter.Tk()
 mode = tkinter.StringVar()
 langlist = tkinter.StringVar()
-list1 = ttk.Combobox(win, textvariable=mode, state='readonly',width=10)
+list1 = ttk.Combobox(win, textvariable=mode, state='readonly', width=10)
 list1.bind("<<ComboboxSelected>>", change)
-list1["value"] = ["A", "B"]
+cur = db.cursor()
+cur.execute("SELECT areaid,areaname FROM area")
+cur_area = cur.fetchall()
+list_area = []
+for i in cur_area:
+    print(i)
+    list_area.append(('%s' + ',' + '%s') % (i[0], i[1]))
+list1["value"] = list_area
 list1.current(0)
-list2=ttk.Combobox(win, textvariable=langlist, state='readonly')
+list2 = ttk.Combobox(win, textvariable=langlist, state='readonly')
 list2.config(values=["you choose A"])
 list2.current(0)
 list1.pack()
